2413ca8f9ed59ea92553aceb4ce03fe01f6d1f9f
Dragsaw <ilya.loiko@mail.ru>
2015-06-08 00:00:22 +0300

    Implemented Repository and UnitOfWork

---
+++ BP.DAL/Concrete/Repository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BP.DAL.Interface.Repositories;
+using BP.ORM;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace BP.DAL.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext context;
+        private readonly DbSet<T> set;
+
+        public Repository(DbContext dbContext)
+        {
+            context = dbContext;
+            set = context.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return set.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return set.AsEnumerable();
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return set.Where(predicate);
+        }
+
+        public void Create(T entity)
+        {
+            set.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            set.Attach(entity);
+            context.Entry<T>(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            Delete(set.Find(id));
+        }
+
+        public void Delete(T entity)
+        {
+            if (context.Entry<T>(entity).State == EntityState.Detached)
+                set.Attach(entity);
+            set.Remove(entity);
+        }
+    }
+}
---
+++ BP.DAL/Concrete/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BP.DAL.Interface.Repositories;
+using BP.DAL.Interface.Entities.Users;
+using BP.DAL.Interface.Entities;
+using BP.ORM;
+using System.Data.Entity;
+using Ninject;
+
+namespace BP.DAL.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext context;
+        private IRepository<DalUser> userRepository;
+        private IRepository<DalRole> roleRepository;
+        private IRepository<DalSkill> skillRepository;
+        private IRepository<DalFilter> filterRepository;
+        private IKernel kernel;
+        private bool disposed;
+
+        public UnitOfWork(DbContext context, IKernel kernel)
+        {
+            this.context = context;
+            this.kernel = kernel;
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            return GetExistingRepository(typeof(T)) as IRepository<T> ?? kernel.Get<IRepository<T>>();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                    context.Dispose();
+            }
+            disposed = true;
+        }
+
+        private object GetExistingRepository(Type type)
+        {
+            if (type == typeof(DalUser))
+                return userRepository;
+            else if (type == typeof(DalRole))
+                return roleRepository;
+            else if (type == typeof(DalSkill))
+                return skillRepository;
+            else return filterRepository;
+        }
+    }
+}
