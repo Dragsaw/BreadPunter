dc3300bc62b89c5dd63cf11a15fb2413ffc2dccf
Dragsaw <ilya.loiko@mail.ru>
2015-06-07 23:57:54 +0300

    Added DependecyResolver project with Ninject

---
+++ BP.DependencyResolver/ResolverSettings.cs
@@ -0,0 +1,24 @@
+﻿using BP.DAL.Concrete;
+using BP.DAL.Interface.Entities;
+using BP.DAL.Interface.Entities.Users;
+using BP.DAL.Interface.Repositories;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BP.DependencyResolver
+{
+    public static class ResolverSettings
+    {
+        public static void Configure(this IKernel kernel)
+        {
+            kernel.Bind<IRepository<DalUser>>().To<Repository<DalUser>>();
+            kernel.Bind<IRepository<DalSkill>>().To<Repository<DalSkill>>();
+            kernel.Bind<IRepository<DalFilter>>().To<Repository<DalFilter>>();
+            kernel.Bind<IKernel>().ToConstant(kernel);
+        }
+    }
+}
