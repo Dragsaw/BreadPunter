eb9cea94fc40c461f5161c479c949da50b0605c8
Dragsaw <ilya.loiko@mail.ru>
2015-06-13 17:42:38 +0300

    Generated base, added packages, configured Ninject

---
+++ BP.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using BP.BLL.Interface.Entities;
+using BP.BLL.Interface.Entities.Users;
+using BP.BLL.Interface.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BP.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BP.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BP.DependencyResolver;
+
+namespace BP.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            kernel.Configure();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
