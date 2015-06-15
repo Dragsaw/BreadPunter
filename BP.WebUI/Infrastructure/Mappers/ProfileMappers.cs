2310fcc91b607560527c99603c51c12953feb8f7
Dragsaw <ilya.loiko@mail.ru>
2015-06-15 19:43:59 +0300

    Added profile view models & mappers

---
+++ BP.WebUI/Infrastructure/Mappers/ProfileMappers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BP.WebUI.Models;
+using BP.BLL.Interface.Entities;
+
+namespace BP.WebUI.Infrastructure.Mappers
+{
+    public static class ProfileMappers
+    {
+        public static FilterSkillViewModel ToMvc(this BalSkill skill)
+        {
+            return new FilterSkillViewModel
+            {
+                Skill = new UserSkillViewModel { Skill = skill }
+            };
+        }
+
+        public static BalFilter ToBal(this CreateFilterViewModel filter)
+        {
+            return new BalFilter
+            {
+                LastViewed = null,
+                Skills = filter.Skills.Where(x => x.Include).ToDictionary(k => k.Skill.Skill, v => v.Skill.Level)
+            };
+        }
+    }
+}
---
+++ BP.WebUI/Models/ProfileModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BP.BLL.Interface.Entities;
+using BP.BLL.Interface.Entities.Users;
+
+namespace BP.WebUI.Models
+{
+    public class FilterSkillViewModel
+    {
+        public UserSkillViewModel Skill { get; set; }
+        public bool Include { get; set; }
+    }
+
+    public class CreateFilterViewModel
+    {
+        public List<FilterSkillViewModel> Skills { get; set; }
+    }
+
+    public class UserSkillViewModel
+    {
+        public BalSkill Skill { get; set; }
+        public int Level { get; set; }
+    }
+
+    public class EditUserSkillsViewModel
+    {
+        public int Id { get; set; }
+        public List<UserSkillViewModel> Skills { get; set; }
+    }
+}
